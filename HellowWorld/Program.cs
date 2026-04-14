6b9ce294c5d3534800352b2cc830da1f078d47ef
A Klitschko <sebasjulio10@hotmail.com>
2026-04-13 19:30:46 -0500

    change hello world by class

--- HellowWorld/Program.cs
+++ HellowWorld/Program.cs
@@ -1,3 +1,15 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+     class Program{
+
+        
+        static void Main()
+    {
+        const string Name="Jhon";
+        Console.WriteLine($"Hello World! {Name}");
+        Console.WriteLine();
+    }
+     }
+}
