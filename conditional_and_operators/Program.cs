d2658799c4eea7ac6370e9499fc8eb4efb034071
A Klitschko <sebasjulio10@hotmail.com>
2026-04-13 20:55:02 -0500

    Condicionals, operators and cone in git

---
+++ conditional_and_operators/Program.cs
@@ -0,0 +1,20 @@
+﻿
+/*conditionals*/
+/**Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine());
+
+if(num >0) Console.WriteLine("Positivo");
+else if(num < 0) Console.WriteLine("Negativo");
+else Console.WriteLine("Es cero");**/
+/*end contiionals*/
+
+/*Operators*/
+
+Console.WriteLine("Ingrese el primero número: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo número: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Suma: {a+b}, Resta: {a-b}, Multiplicación: {a*b}, División: {(b!=0 ? a/b : "Indeterminado")}");
+
+
