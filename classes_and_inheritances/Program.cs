c3ac0d977e867724da5e6f651dab5952feb8d04f
A Klitschko <sebasjulio10@hotmail.com>
2026-04-13 21:10:26 -0500

    Add Rectangulo and Cuadrado classes with area and perimeter methods

---
+++ classes_and_inheritances/Program.cs
@@ -0,0 +1,27 @@
+﻿var cuadr = new Cuadrado();
+cuadr.Base = 20;
+cuadr.Altura = 40;
+
+Console.WriteLine($"EL area es : {cuadr.Area()}");
+Console.WriteLine($"EL perimetro es : {cuadr.Perimetro()}");
+Console.WriteLine($"EL lado es : {cuadr.Lado}");
+
+
+class Rectangulo
+{
+    public double Base { get; set; }
+    public double Altura { get; set; }
+
+    public double Area() => Base * Altura;
+
+    public double Perimetro() => 2 * Base + 2 * Altura;
+}
+
+class Cuadrado : Rectangulo
+{
+    public double Lado
+    {
+        set { Base = value; Altura = value; }
+        get { return Base; }
+    }
+}
